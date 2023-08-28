947912839223ceb1a9607a0b4aa059e1c60dbf85
arnob016 <arnob.social@gmail.com>
2023-08-28 10:27:06 +0600

    nav issue fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
-import {style, styles} from '../styles';
+import {styles} from '../styles';
 import { navLinks } from '../constants';
 import {logo, menu, close} from '../assets';
 
