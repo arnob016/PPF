fbdd60f29e3d15dac60ce4f885c72a1ba9229717
arnob016 <arnob.social@gmail.com>
2023-08-30 09:39:47 +0600

    bg update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
 
   return (
     <nav className={`
-    ${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+    ${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-[#00365c]`}>
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
       <Link to="/"
       className="flex items-center gap-2"
