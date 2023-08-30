149f6bc35dd2cd6bf2d1843c96a7cf1057453c00
arnob016 <arnob.social@gmail.com>
2023-08-30 09:28:48 +0600

    Scrren size fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,25 +25,27 @@ const Hero = () => {
       <div className={`ml-4 w-full pl-4 px-4 ${styles.visible}`}>
         <ThreeObject/>
         </div>
-        </div>
-        </div>
-    <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
-            <a href='#about'>
-              <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
-                <motion.div
-                  animate={{
-                    y: [0, 24, 0],
-                  }}
-                  transition={{
-                    duration: 1.5,
-                    repeat: Infinity,
-                    repeatType: "loop",
-                  }}
-                  className='w-3 h-3 rounded-full bg-secondary mb-1'
-                />
-              </div>
-            </a>
-          </div>      
+  </div>
+  </div>
+    
+
+<div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        <a href='#about'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
+            />
+          </div>
+        </a>
+      </div>      
 
     </section>
   )
