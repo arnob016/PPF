3f7a65f237217eb6126fc7335f5a1a225a4dcaf0
arnob016 <arnob.social@gmail.com>
2023-08-30 09:11:53 +0600

    mouse animation added

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,11 @@ module.exports = {
       },
       screens: {
         xs: "450px",
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+        ext: ''
       },
       backgroundImage: {
         "hero-pattern": "url('/src/assets/herobg.png')",
