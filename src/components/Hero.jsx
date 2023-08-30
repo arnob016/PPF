fc24d749d45d04434b03b31b797bf6ebf2b7c2e4
arnob016 <arnob.social@gmail.com>
2023-08-30 09:12:07 +0600

    mouse animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,16 +20,31 @@ const Hero = () => {
           <p className={`${styles.heroSubText} mt-2 text-white-100`}> Welcome to my portfolio!</p>
           <div className='md:20 xl:20 lg:h-20'></div>
         </div>
+      
       <div className={`${styles.visible}`}></div>
       <div className={`ml-4 w-full pl-4 px-4 ${styles.visible}`}>
         <ThreeObject/>
-      </div>
-      </div>
+        </div>
+        </div>
+        </div>
+    <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+            <a href='#about'>
+              <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+                <motion.div
+                  animate={{
+                    y: [0, 24, 0],
+                  }}
+                  transition={{
+                    duration: 1.5,
+                    repeat: Infinity,
+                    repeatType: "loop",
+                  }}
+                  className='w-3 h-3 rounded-full bg-secondary mb-1'
+                />
+              </div>
+            </a>
+          </div>      
 
-
-      </div>
-            
-      
     </section>
   )
 }
--- src/styles.js
+++ src/styles.js
@@ -2,8 +2,8 @@ const styles = {
     paddingX: "sm:px-16 px-6",
     paddingY: "sm:py-16 py-6",
     padding: "sm:px-16 px-6 sm:py-16 py-10",
-    visible: "hidden lg:flex lg:visible xl:flex xl:visible",
-    invisible: "hidden xs:visible xs:flex md:visible md:flex sm:visible sm:flex",
+    visible: "hidden xl:flex xl:visible",
+    invisible: "hidden xs:visible xs:flex sm:visible sm:flex",
     heroHeadText:
       "font-black text-white lg:text-[60px] sm:text-[40px] xs:text-[30px] text-[20px] lg:leading-[98px] mt-2",
     heroSubText:
