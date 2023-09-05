24708ea05fbbe4905c2246d716945b530f8cec77
arnob016 <arnob.social@gmail.com>
2023-09-05 10:59:20 +0600

    Mini screen 3d object fix

--- src/styles.js
+++ src/styles.js
@@ -3,7 +3,7 @@ const styles = {
     paddingY: "sm:py-16 py-6",
     padding: "sm:px-16 px-6 sm:py-16 py-10",
     visible: "hidden lg:flex lg:visible",
-    invisible: "hidden xs:visible xs:flex sm:visible sm:flex md:flex md:visible lg:flex lg:hidden",
+    invisible: "block lg:flex lg:hidden",
     heroHeadText:
       "font-black text-white lg:text-[60px] sm:text-[50px] xs:text-[30px] text-[20px] lg:leading-[98px] mt-2",
     heroSubText:
