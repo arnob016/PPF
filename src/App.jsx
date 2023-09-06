be6b9fef37fe9d96db2ed7773562b5bca937796d
arnob016 <arnob.social@gmail.com>
2023-09-06 22:06:19 +0600

    push no techstack check

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,7 @@ import LoadingScreen from "./components/LoadingScreen";
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas, ProjectCard } from "./components";
 
 const App = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [loadingProgress, setLoadingProgress] = useState(0);
 
-  useEffect(() => {
-    const loadingInterval = setInterval(() => {
-      setLoadingProgress((prevProgress) => {
-        const newProgress = prevProgress + 5;
-        if (newProgress >= 100) {
-          clearInterval(loadingInterval);
-          setIsLoading(false);
-          return 100;
-        }
-        return newProgress;
-      });
-    }, 200);
-
-    return () => {
-      clearInterval(loadingInterval);
-    };
-  }, []);
 
   return (
     <BrowserRouter>
@@ -33,10 +14,7 @@ const App = () => {
         <Navbar />
         <Hero />
         <About />
-
-
       <Experience />
-      <Tech />
       <Works />
       <Feedbacks />
       </div>
