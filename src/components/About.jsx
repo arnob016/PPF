3da5466a4aec0c94d0b8f336c6a6209bb5aff8ab
arnob016 <arnob.social@gmail.com>
2023-09-18 00:49:26 +0600

    dp added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,6 +8,7 @@ import { styles } from "../styles";
 import { services } from "../constants";
 import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
+import ppf from "../assets/ppf.jpg";
 
 const ServiceCard = ({ index, title, icon }) => {
   return (
@@ -42,18 +43,24 @@ const ServiceCard = ({ index, title, icon }) => {
 const About = () => {
   return (
     <>
-       <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}>Introduction</p>
-        <h2 className={styles.sectionHeadText}>Overview.</h2>
+    <div className='flex flex-col items-center gap-10 lg:flex-row'>
+      <div className='flex flex-col justify-center'>
+        <motion.div variants={textVariant()}>
+          <h2 className={styles.sectionHeadText}>Introduction.</h2>
+        </motion.div>
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+        >
+          I&apos;m a software developer with years of experience in project management, blockchain technology. I&apos;ve also gained experience in programming, problem solving, marketing, and scripting. I&apos;m highly skilled in communications, digital storytelling, and UI designing as well.
+        </motion.p>
+      </div>
+      <motion.div variants={textVariant()} className='mt-10'>
+      <img src={ppf} alt='jobit' className='object-contain h-64 border-4 border-transparent rounded-full bg-clip-border bg-gradient-to-r from-green-300 via-yellow-300 to-pink-300'/>
       </motion.div>
-
-      <motion.p
-        variants={fadeIn("", "", 0.1, 1)}
-        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
-      >
-        I&apos;m a software developer with years of experience in project management, blockchain technology. I&apos;ve also gained experience in programming, problem solving, marketing, and scripting. I&apos;m highly skilled in communications, digital storytelling, and UI designing as well.
-      </motion.p>
-
+    </div>
+      
+      
       <div className='flex flex-wrap gap-10 mt-20'>
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
