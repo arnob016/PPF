45e265c97bb98b428fedff538817178d426a0cd5
arnob016 <arnob.social@gmail.com>
2023-09-19 12:41:39 +0600

    giscus test push

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 import {useState, useRef} from 'react';
 import emailjs  from '@emailjs/browser';
 import { SectionWrapper } from "../hoc";
-
+import Giscus from '@giscus/react';
 import {motion} from "framer-motion";
 import {styles} from '../styles'
 import { slideIn } from '../utils/motion';
@@ -80,7 +80,7 @@ const Contact = () => {
     <div className='flex flex-col gap-6 overflow-hidden lg:gap-0 lg:flex-row lg:justify-between'>
       <motion.div
         variants={slideIn('left', "tween", 0.2, 1)}
-        className = "flex-[0.6] bg-black-100 p-8 rounded-2xl">
+        className = "flex-[0.5] bg-black-100 p-8 rounded-2xl">
           <p className="sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider">Let&apos;s get in touch!</p>
           <h3 className="text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px] -mb-6">Contact me</h3>
 
@@ -134,23 +134,31 @@ const Contact = () => {
           </form>
 
         </motion.div>
-        <div className='overflow-hidden lg:flex-[0.3] flex items-center'>
+        <div className='overflow-hidden lg:flex-[0.45] flex items-center'>
           <motion.div
           variants={slideIn('right', "tween", 0.2, 1)}
           className = "items-center justify-center w-full h-auto p-6 bg-black-100 rounded-2xl ">
 
             <div className='flex flex-col items-center gap-4 p-4'>
               <div className='font-extrabold text-white font'>Did you like my portfolio?</div>
-              <div className='flex flex-row justify-center gap-4 font-extrabold'>
-              <div className='cursor-pointer hover:-translate-x-2 hover:-translate-y-1 hover:scale-150 hover:animate-pulse '>😍</div>
-                <div className='cursor-pointer hover:-translate-x-2 hover:-translate-y-1 hover:scale-150 hover:animate-pulse'>😄</div>
-                <div className='cursor-pointer hover:-translate-x-2 hover:-translate-y-1 hover:scale-150 hover:animate-pulse '>😕</div>
-                <div className='cursor-pointer hover:-translate-x-2 hover:-translate-y-1 hover:scale-150 hover:animate-pulse '>😞</div>
-                <div className='cursor-pointer hover:-translate-x-2 hover:-translate-y-1 hover:scale-150 hover:animate-pulse '>🤢</div>
-              </div>
+              <div className='font-extrabold text-white font'>Leave a comment and reaction </div>
 
-          <div>
-            <textarea name="textarea" id="comment" cols="30" rows="2" placeholder='Leave a comment.' className='flex justify-center w-full p-2 rounded-lg'></textarea>
+          <div className='w-full max-w-5xl'>
+          <Giscus
+      id="comments"
+      repo="arnob016/PPF"
+      repoId="R_kgDOKLcLmg"
+      category="General"
+      categoryId="DIC_kwDOKLcLms4CZbVb"
+      mapping="pathname"
+      term="Welcome to @giscus/react component!"
+      reactionsEnabled="1"
+      emitMetadata="0"
+      inputPosition="top"
+      theme="purple_dark"
+      lang="en"
+      loading="lazy"
+    />
           </div>
             </div>
           </motion.div>
